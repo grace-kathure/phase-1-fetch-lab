17db73d0e6ac2e70c2f5ed70bcddfc92b20b4935
grace-kathure <gracegikundak@gmail.com>
2022-12-15 17:33:17 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 function fetchBooks() {
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(response => response.json())
+  .then((books) => renderBooks(books))
+    
   // To pass the tests, don't forget to return your fetch!
   
 }
+// fetchBooks()
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
